827256c72e09061c003512fecd094012c45b1437
Paula <paulaagarcia1@gmail.com>
2018-09-24 15:19:41 -0500

    firebase config ts file

---
+++ config/firebaseConfig.ts
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase'
+// Required for side-effects
+require("firebase/firestore");
+
+const config = {
+    apiKey: "AIzaSyBeCxe5RUXEogsI_Zqd6igbmQvuJazYCe0",
+    authDomain: "todolist-react-firestore.firebaseapp.com",
+    databaseURL: "https://todolist-react-firestore.firebaseio.com",
+    projectId: "todolist-react-firestore",
+    storageBucket: "todolist-react-firestore.appspot.com",
+    messagingSenderId: "601485435019"
+  };
+  
+firebase.initializeApp(config);
+  
+const db = firebase.firestore();
+
+export default db;
