6c8688597cdecd3a1df420bb2206bbf9f00373cd
Santimondra <santiagomondram@gmail.com>
2018-11-30 14:13:37 -0500

    Auth merged

--- src/containers/Login/Login.tsx
+++ src/containers/Login/Login.tsx
@@ -4,10 +4,20 @@ import { observer } from 'mobx-react';
 import { withRouter } from 'react-router';
 import LoginForm from '../../components/Common/LoginForm/LoginForm';
 import '../Login/Login.scss';
+import { authStore } from '../../store/AuthStore';
 
-interface LoginProps {}
+interface LoginProps {
+    history : any
+}
 
 @observer export class Login extends React.Component<LoginProps> {
+
+    constructor(props: any) {
+        super(props);
+        if(authStore.isLogged){
+            props.history.push ("/home");
+        }
+    }
 
     render() {
         return (
--- src/containers/Register/Register.tsx
+++ src/containers/Register/Register.tsx
@@ -1,9 +1,20 @@
 import * as React from 'react';
 import { observer } from 'mobx-react';
 import RegisterForm from '../../components/Common/RegisterForm/RegisterForm';
+import { authStore } from '../../store/AuthStore';
 
+interface RegisterProps {
+    history : any
+}
 
-@observer class Register extends React.Component {
+@observer class Register extends React.Component<RegisterProps> {
+
+    constructor(props: any) {
+        super(props);
+        if(authStore.isLogged){
+            props.history.push ("/home");
+        }
+    }
     render() {
         return <section className="formC">
             <RegisterForm />
