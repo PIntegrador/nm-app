826f7d1814f6fcf746bd45f13c004b6b6df03dfa
polagmnoz <paulaagarcia1@gmail.com>
2018-12-03 10:19:24 -0500

    final

--- src/components/Common/LoginForm/LoginForm.tsx
+++ src/components/Common/LoginForm/LoginForm.tsx
@@ -44,16 +44,16 @@ const LoginForm = observer(withRouter((formLoginProps) => {
                     onChange={(e:any) => {
                         authStore.credentials.password = e.target.value;
                     }} />
+                
                 <button
+                    id="toLogin"
+                    type="submit"
+                >Ingresar</button><button
                     id="toRegister"
                     onClick={() => {
                         formLoginProps.history.push("/register");
                     }}
                 >No tengo una cuenta, deseo registrarme</button>
-                <button
-                    id="toLogin"
-                    type="submit"
-                >Ingresar</button>
                 <p>Al continuar, aceptas las <a>Condiciones del servicio</a> y la <a>Política de privacidad.</a></p>
             </form>
         </article>
